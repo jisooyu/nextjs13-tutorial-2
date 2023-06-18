43ba19b24c74d440895322e861c761e4cbcb4e0b
jisooyu <jisooyu@hotmail.com>
2023-06-18 14:30:55 +0900

    changing metadaa and title in Home

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 
 export const metadata = {
-	title: 'NextJS 13 tutorial 1',
-	description: 'nextjs 13 tutorial 1 - authentication',
+	title: 'NextJS 13 tutorial 2',
+	description:
+		'nextjs 13 tutorial 2 - creating and posting prompts using next-auth and mongoose',
 };
 
 export default function Home() {
 	return (
 		<main className='feed text-blue-900 font-bold'>
-			<h1>Authentication of Next.js using Next-Auth</h1>
-			<p>NextJS 13 Tutorial 1 : Authentication</p>
+			<h1>Creating and posting prompts of Next.js</h1>
+			<p>NextJS 13 Tutorial 2 : creating and posting prompts</p>
 		</main>
 	);
 }
